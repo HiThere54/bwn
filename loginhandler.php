ea813768f27bff2c8aa07d9727d1521b4e037305
HiThere54 <96961737+HiThere54@users.noreply.github.com>
2022-07-19 16:50:12 +0000

    HiThere54

---
+++ loginhandler.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<?php
+if ($_POST["pwd"] == "0612") {
+    header("Location: https://secret.boogiewoogie.ga/");
+} else {
+    echo "Wrong password";
+}
+
+?>
+
+
+</body>
+</html> 
